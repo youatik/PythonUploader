b149e7e898b1354fa1c565cc62c94c5e1c6eb734
youatik <y_ouatik@hotmail.com>
2023-10-14 12:31:57 -0400

    Changed database for SQLite

--- main.py
+++ main.py
@@ -1,44 +1,52 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
+from sqlalchemy import create_engine, Column, Integer, String, LargeBinary
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import os
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 
-# Define the upload folder where the files will be saved
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# SQLite database configuration
+DATABASE_URI = 'sqlite:///file_storage.db'  # Change this to your desired database URI
+engine = create_engine(DATABASE_URI)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
 
-# Define the allowed file extensions (e.g., for images, you can add 'jpg', 'jpeg', 'png', etc.)
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+# Define the SQLAlchemy model for the file table
+class File(Base):
+    __tablename__ = 'files'
 
+    id = Column(Integer, primary_key=True)
+    filename = Column(String(255), nullable=False)
+    data = Column(LargeBinary, nullable=False)
 
-# Function to check if a file extension is allowed
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# Create the table in the database (Run this once to initialize the database)
+Base.metadata.create_all(engine)
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
 
-@app.route('/upload')
-def upload_form():
+        if uploaded_file and allowed_file(uploaded_file.filename):
+            filename = secure_filename(uploaded_file.filename)
+            file_data = uploaded_file.read()
+
+            # Store the file in the database
+            session = Session()
+            new_file = File(filename=filename, data=file_data)
+            session.add(new_file)
+            session.commit()
+            session.close()
+
+            return 'File uploaded successfully.'
+
     return render_template('upload.html')
 
-
-@app.route('/uploader', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return 'No file part'
-
-    file = request.files['file']
-
-    if file.filename == '':
-        return 'No selected file'
-
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return 'File uploaded successfully'
-
-    return 'Invalid file format'
-
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == '__main__':
     app.run(debug=True)
